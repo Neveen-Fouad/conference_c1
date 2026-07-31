f70a8a37915fa70dc4e512a84687af6fbd83dc80
sarah-548 <sara70648@gmail.com>
2026-07-31 22:43:07 +0300

    first com

---
+++ database/migrations/2026_07_31_161625_create_trips_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId("user_id")->constrained();
+            $table->string("classes");
+            $table->string("destination");
+            $table->int("number_of_travels");
+            $table->decimal("estimated_expenses");
+            //$table->string("transport_tips");
+            $table->decimal("budget");
+            $table->string("attractions");
+            $table->int("days");
+            //$table->string("style");
+
+
+
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trips');
+    }
+};
