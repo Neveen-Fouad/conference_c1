5e77f5615152234937686c43a1e091d3aeaa426e
Mohandayman173 <mohandayman173@gmail.com>
2026-08-08 19:34:54 +0300

    add notfication route

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\RevenueController;
-
+use App\Http\Controllers\NotificationsController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
@@ -180,5 +180,11 @@ Route::middleware('auth:api')->group(function(){
     Route::delete('/favourites/{favourite_id}',[FavouritesController::class,'destroy']);
 });
 Route::get('/revenue/total', [RevenueController::class, 'totalRevenue']);
+Route::post('/notifications', [NotificationsController::class, 'store']);
+Route::get('/notifications/client/{clientId}', [NotificationsController::class, 'index']);
+Route::get('/notifications/client/{clientId}/unread', [NotificationsController::class, 'unread']);
+Route::get('/notifications/client/{clientId}/unread-count', [NotificationsController::class, 'unreadCount']);
+Route::patch('/notifications/{notificationId}/read', [NotificationsController::class, 'markAsRead']);
+Route::patch('/notifications/client/{clientId}/read-all', [NotificationsController::class, 'markAllAsRead']);
 
 
