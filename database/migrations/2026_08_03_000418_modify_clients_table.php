2c22cc3f0755c72f23b48cbc670c2902ff62b6d4
Neveen-Fouad <neveenfouad73@gmail.com>
2026-08-09 15:23:04 +0300

    removing drop interest from modify clients table because it is repeated

--- database/migrations/2026_08_03_000418_modify_clients_table.php
+++ database/migrations/2026_08_03_000418_modify_clients_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::table('clients', function (Blueprint $table) {
         $table->dropColumn('location');
-        $table->dropColumn('interest');
         $table->string('long');
         $table->string('latittude');
         $table->date('birth_date')->change();
